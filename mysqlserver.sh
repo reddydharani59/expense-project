0c55a5b352a8338b77d27164489ed6461167d3fe
Madhav reddy <reddydharani59@gmail.com>
2024-04-19 15:37:49 +0530

    installing packages

--- mysqlserver.sh
+++ mysqlserver.sh
@@ -26,10 +26,12 @@ VALIDATE()
 }
 dnf install mysql-server -y &>>$LOGFILE
 VALIDATE $? "Installing Mysql-Server"
+
 systemctl start mysqld &>>$LOGFILE
 VALIDATE $? "starting mysql-server"
+
 systemctl enable mysqld &>>$LOGFILE
-VALIDATE $? "enabling mysql-server "
+VALIDATE $? "enabling mysql-server"
 
 mysql-h db.daws-78s.site -uroot -p${mysql-passwd} &>>$LOGFILE
 # if [ $? ne 0 ]
@@ -40,9 +42,9 @@ mysql-h db.daws-78s.site -uroot -p${mysql-passwd} &>>$LOGFILE
   #   echo "Already you set mysql password read to go"
  # fi
  if [ $? -ne 0 ]
-then
+  then
     mysql_secure_installation --set-root-pass ${mysql_root_password} &>>$LOGFILE
     VALIDATE $? "MySQL Root password Setup"
-else
+ else
     echo -e "MySQL Root password is already setup...$Y SKIPPING $N"
-fi
+ fi
