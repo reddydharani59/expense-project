37f736e3ff83bb1d635a57af782c12afc065a33a
Madhav reddy <reddydharani59@gmail.com>
2024-04-19 15:10:56 +0530

    installing packages

---
+++ mysqlserver.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+USER=$(id -u)
+TIMESTAMP=$(date +%F-%H-%M-%S)
+SCRIPTNAME=$(echo $? | cut -d ".' -f1)
+LOGFILE=/tmp/$SCRIPTNAME.$TIMESTAMP.log
+echo "enter mysql-password"
+read -s mysql_passwd
+R="/e[31M"
+y="e[32m"
+B="/e[33m"
+if [ $USER ne 0]
+then 
+echo -e " $R Get super user access"
+else 
+echo -e "$Y you are super user proceed"
+VALIDATE()
+{
+    if [ $1 eq 0 ]
+    then
+      echo -e "$2 .....is $B success"
+    else 
+      echo -e "$2 ....$R is failure"
+    fi
+
+}
+dnf install mysql-server -y &>>$LOGFILE
+VALIDATE $? "Installing Mysql-Server"
+systemctl start mysqld &>>$LOGFILE
+VALIDATE $? "starting mysql-server"
+systemctl enable mysqld &>>$LOGFILE
+VALIDATE $? "enabling mysql-server "
+
+mysql-h db.daws-78s.site -uroot -p${mysql-passsword} &>>$LOGFILE
+if [ $? ne 0]
+then 
+ mysql_secure_installation --set-root-pass ${mysql-password} &>>$LOGFILE
+ fi
