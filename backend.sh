ed47bd6d5bec5c46cec107eb97370172a72b1cbe
Madhav reddy <reddydharani59@gmail.com>
2024-04-21 05:42:06 +0530

    setting configurations

--- backend.sh
+++ backend.sh
@@ -57,7 +57,7 @@ VALIDATE $? "Extracted backend code"
 npm install &>>$LOGFILE
 VALIDATE $? "Installing nodejs dependencies"
 
-cp /home/ec2-user/expense-shell/backendservice /etc/systemd/system/backend.service &>>$LOGFILE
+cp /home/ec2-user/expense-shell/backendservice.sh /etc/systemd/system/backend.service &>>$LOGFILE
 VALIDATE $? "Copied backend service"
 
 systemctl daemon-reload &>>$LOGFILE
